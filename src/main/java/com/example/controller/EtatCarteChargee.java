b3bbfa65a02c9fd1b8171759bd228aa53529bcda
LapinRobin <61903127+LapinRobin@users.noreply.github.com>
2023-10-24 16:06:13 +0200

    Create skeleton for EtatCarteChargee

--- src/main/java/com/example/controller/EtatCarteChargee.java
+++ src/main/java/com/example/controller/EtatCarteChargee.java
@@ -11,4 +11,34 @@ public class EtatCarteChargee implements Etat {
     public EtatCarteChargee() {
     }
 
+    /**
+     * Charger des demandes.
+     * @param c Controller instance.
+     * // TODO: Add other necessary parameters if needed.
+     */
+    public void chargerDesDemandes(Controller c) {
+        // Implement the logic to load demands.
+        // If loading is successful, you may want to transition to a new state.
+    }
+
+    /**
+     * Modifier le nombre de coursiers.
+     * @param c Controller instance.
+     * @param numberOfCouriers The new number of couriers.
+     */
+    public void modifierNombreDeCoursiers(Controller c, int numberOfCouriers) {
+        // Implement the logic to modify the number of couriers.
+    }
+
+    /**
+     * Créer une nouvelle demande.
+     * @param c Controller instance.
+     * // TODO: Add other necessary parameters if needed.
+     */
+    public void creerUneDemande(Controller c) {
+        // Implement the logic to create a new request.
+        // After creating a request, you may want to transition to the "Demande ajoutée" state.
+    }
+
+    // You can add other methods or transitions based on the logic you need.
 }
