d08fd1a287279558ed6364bb95baff78883b3d19
treelins <105914233+treelins@users.noreply.github.com>
2023-11-01 19:17:07 +0100

    Arrow and overlap area

--- src/main/java/com/example/agiledelivery/GraphicalView.java
+++ src/main/java/com/example/agiledelivery/GraphicalView.java
@@ -3,15 +3,21 @@ package com.example.agiledelivery;
 import com.example.model.*;
 
 import javafx.collections.ObservableMap;
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
-
+import javafx.scene.shape.StrokeType;
+import javafx.util.Pair;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class GraphicalView extends Pane implements PropertyChangeListener, Visitor{
 
@@ -127,13 +133,10 @@ public class GraphicalView extends Pane implements PropertyChangeListener, Visit
         double scaleX = graph.getWidth() / rangeLon;
         double scaleY = graph.getHeight() / rangeLat;
         double scale = Math.min(scaleX, scaleY);
-
+        HashSet<Pair<Long, Long>> hashSet = new HashSet<>();
         for(Chemin chemin : tournee.getListeChemins())
         {
-            // Set a different color for each path
-            double r = 1.0;
-            double g = 0.0;
-            double b = 0.0;
+
             for (Segment segment : chemin.getListeSegments()) {
                 double adjustedX1 = (segment.getOrigin().getLongitude() - midLon) * scale + graph.getWidth() / 2;
                 double adjustedY1 = -(segment.getOrigin().getLatitude() - midLat) * scale + graph.getHeight() / 2;
@@ -152,13 +155,21 @@ public class GraphicalView extends Pane implements PropertyChangeListener, Visit
                 arrowhead.setFill(Color.BLACK);
 
                 Line line = new Line(adjustedX1, adjustedY1, adjustedX2, adjustedY2);
-                line.setStyle("-fx-stroke: rgb(" + r * 255 + "," + g * 255 + "," + b * 255 + ");");
-                // change line width
-                line.setStrokeWidth(3);
-                graph.getChildren().add(line); // Add to right pane
-                graph.getChildren().add(arrowhead); // Add to right pane
-            }
 
+                line.setStrokeType(StrokeType.OUTSIDE); // Set the stroke type
+                if(hashSet.contains(new Pair<>(segment.getOrigin().getId(), segment.getDestination().getId())) || hashSet.contains(new Pair<>(segment.getDestination().getId(), segment.getOrigin().getId()))) {
+                    System.out.println("123");
+                    line.setStroke(Color.RED);
+                }
+                else applyCustomStroke(line, Color.BLUE, Color.VIOLET);
+                hashSet.add(new Pair<>(segment.getOrigin().getId(), segment.getDestination().getId()));
+                graph.getChildren().add(line);
+                graph.getChildren().add(arrowhead);
+            }
         }
+    }
+
+    private void applyCustomStroke(Line line, Color startColor, Color endColor) {
+        line.setStroke(new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE, new Stop(0, startColor), new Stop(1, endColor)));
     }
 }
