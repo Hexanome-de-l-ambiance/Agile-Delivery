9d4f78c62d6e7afb47079ca1a100160cd19f70a4
celiadjouadi <celia.djouadi@insa-lyon.fr>
2023-11-14 08:21:14 +0100

    tests pour la classe Distance

---
+++ test/java/com/example/utils/DistanceTest.java
@@ -0,0 +1,26 @@
+package com.example.model;
+
+import com.example.utils.Distance;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistanceTest {
+
+    @Test
+    void testHaversine() {
+        // Test à partir de coordonnées connues et d'une distance attendue
+        double lat1 = 40.0;
+        double lon1 = -74.0;
+        double lat2 = 41.0;
+        double lon2 = -75.0;
+
+        // Distance attendue en mètres
+        double distanceAttendue = 150000.0;
+
+        double distanceCalcule = Distance.haversine(lat1, lon1, lat2, lon2);
+
+        // Verification de l'égalité entre la distance attendue et la distance calculée à un mètre près
+        assertEquals(distanceAttendue, distanceCalcule, 1.0);
+    }
+
+}
