50d6614966d69537b8343a5abd81ec0a9de02acc
treelins <105914233+treelins@users.noreply.github.com>
2023-11-15 11:55:36 +0100

    commentations for javadoc

--- src/main/java/com/example/controller/Etat.java
+++ src/main/java/com/example/controller/Etat.java
@@ -8,19 +8,43 @@ import javafx.stage.Stage;
 import java.time.LocalTime;
 
 /**
- *
+ * L'interface Etat définit les méthodes disponibles pour chaque état du contrôleur.
  */
 public interface Etat {
 
+    /**
+     * Ajoute une livraison à un coursier avec une heure spécifiée, avant le calcul des tournées.
+     *
+     * @param l             La liste de commandes
+     * @param heure         L'heure de livraison
+     * @param numeroCoursier Le numéro du coursier
+     * @param c             Le contrôleur
+     * @param carte         La carte
+     */
     public default void addDelivery(ListeDeCommandes l, LocalTime heure, int numeroCoursier, Controller c, Carte carte){};
+
+    /**
+     * Ajoute une livraison à un coursier avec une heure et un index spécifiés, après le calcul des tournées..
+     *
+     * @param l             La liste de commandes
+     * @param heure         L'heure de livraison
+     * @param numeroCoursier Le numéro du coursier
+     * @param index         L'index de la livraison
+     * @param c             Le contrôleur
+     * @param carte         La carte
+     */
     public default void addDelivery(ListeDeCommandes l, LocalTime heure, int numeroCoursier, int index, Controller c, Carte carte){};
+
+    /**
+     * Ajouter une intersection pour l'ajout d'une livraison.
+     *
+     * @param intersection L'intersection à ajouter
+     */
     public default void addIntersection(Controller c, Intersection intersection) {};
     public default void deleteDelivery(ListeDeCommandes l, int numeroCoursier, Livraison livraison, Controller c, Carte carte){};
     public default void deleteDelivery(ListeDeCommandes l, int numeroCoursier, Livraison livraison, int index, Controller c, Carte carte){};
     public default void calculerLivraisons(ListeDeCommandes l, Controller c, Carte carte) {}
-
     public default void modifierCoursiers(Controller c, Carte carte, int nombre) {}
-
     public default void undo(ListeDeCommandes l){};
     public default void redo(ListeDeCommandes l){};
     public default void loadMap(Controller c, Carte carte, ListeDeCommandes l, Stage stage){};
@@ -28,7 +52,6 @@ public interface Etat {
     public default void loadTour(Controller c, Carte carte, Stage stage){};
     public default void saveTour(Controller c, Carte carte, Stage stage){};
     public default void unselectIntersection(Controller c){};
-
     public default void genererFeuilleRoute(Controller c, Carte carte){};
 
 }
